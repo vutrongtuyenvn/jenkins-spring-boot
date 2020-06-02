d6b5e51ce8170bace76f4b702ace28d87f0c6fea
vutrongtuyenvn <32796011+vutrongtuyenvn@users.noreply.github.com>
2020-06-02 14:43:35 +0700

    abc

---
+++ src/main/java/com/jenkins/core/controller/JenkinsController.java
@@ -0,0 +1,16 @@
+package com.jenkins.core.controller;
+
+
+import com.google.gson.Gson;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/business")
+public class JenkinsController {
+    @GetMapping
+    private java.lang.String helloWorld() {
+        return new Gson().toJson("hello world");
+    }
+}
